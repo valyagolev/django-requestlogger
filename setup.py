d87cc1e7b790aff778fb57281a6f99f2a69178ee
Valya Golev <me@valyagolev.net>
2011-06-23 20:22:07 +0200

    setup.py!

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='django-requestlogger',
+      version='0.1',
+      description="Plug'n'play request logger for Django",
+      author='Valentin Golev',
+      author_email='v.golev@gmail.com',
+      url='https://github.com/va1en0k/django-requestlogger',
+      packages=['requestlogger'],
+     )
