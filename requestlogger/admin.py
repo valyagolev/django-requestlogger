f6f0ae54ba41926aa18b064c46337d39dc3ab160
Valya Golev <me@valyagolev.net>
2011-06-27 19:23:52 +0200

    admin interface with cute aggregations

--- requestlogger/admin.py
+++ requestlogger/admin.py
@@ -1,9 +1,25 @@
 from django.contrib import admin
+from django.db.models import Avg, Max, Count
 
-from .models import Request
+from .models import Request, View
 
 class RequestAdmin(admin.ModelAdmin):
-    list_display = ['method', 'path', 'datetime', 'response_time', 'status_code', 'exception_class', 'exception_message', 'view_module', 'view_func']
-    list_filter = ['path', 'method', 'status_code', 'exception_class', 'view_module', 'view_func']
+    list_display = ['method', 'path', 'datetime', 'response_time', 'status_code', 'exception_class', 'exception_message', 'view']
+    list_filter = ['path', 'method', 'status_code', 'exception_class', 'view__module', 'view__func']
+
+class ViewAdmin(admin.ModelAdmin):
+    list_display = ['module', 'func',
+                    'avg_response_time',
+                    'max_response_time',
+                    'request_count']
+
+    def queryset(self, *args, **kwargs):
+        qs = super(ViewAdmin, self).queryset(*args, **kwargs)
+        qs = qs.annotate(avg_response_time=Avg('request__response_time'),
+                         max_response_time=Max('request__response_time'),
+                         request_count=Count('request'))        
+        return qs
+
 
 admin.site.register(Request, RequestAdmin)
+admin.site.register(View, ViewAdmin)
--- requestlogger/models.py
+++ requestlogger/models.py
@@ -9,11 +9,30 @@ def get_view_representation(view_func):
     return View.objects.get_or_create(module=module,
                                       func=func)[0]
 
+
+def aggregated_column(name):
+    def _func(self):
+        assert False
+
+    _func.__name__ = name
+    _func.admin_order_field = name
+    
+    return _func
+
+
 class View(models.Model):
     """A view representation, defined by module and func names"""
 
     module = models.CharField(max_length=255, blank=True)
     func = models.CharField(max_length=255, blank=True)
+
+    # these all are failing since they should be set via queryset
+    # but django's 'ModelAdmin' insists on having such functions
+    # in the model
+    avg_response_time = aggregated_column('avg_response_time')
+    max_response_time = aggregated_column('max_response_time')
+    request_count = aggregated_column('request_count')
+
 
     class Meta:
         unique_together = [('module', 'func')]
